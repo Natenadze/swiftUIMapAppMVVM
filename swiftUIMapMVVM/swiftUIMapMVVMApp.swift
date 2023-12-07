f6c3b9b838d972817438562564ab90024f565cdb
Davit Natenadze <davit.natenadze@gmail.com>
2023-12-07 13:56:55 +0100

    Initial Commit

---
+++ swiftUIMapMVVM/swiftUIMapMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIMapMVVMApp.swift
+//  swiftUIMapMVVM
+//
+//  Created by Davit Natenadze on 07.12.23.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIMapMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
